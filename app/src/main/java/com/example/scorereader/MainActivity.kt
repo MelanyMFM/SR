fdb1934d41151ed88904aa790cea6847ecca9cea
MelanyMFM <mefrancom@unal.edu.co>
2026-03-04 13:50:27 -0500

    app setup

---
+++ app/src/main/java/com/example/scorereader/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.scorereader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.scorereader.ui.theme.ScoreReaderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScoreReaderTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
