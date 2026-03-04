7e5370729207918dd2eec04e0509146d5647ce79
MelanyMFM <mefrancom@unal.edu.co>
2026-03-04 17:53:35 -0500

    initial annotations

--- app/src/main/java/com/example/scorereader/ui/reader/ReaderScreen.kt
+++ app/src/main/java/com/example/scorereader/ui/reader/ReaderScreen.kt
@@ -20,6 +20,14 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.collectAsState
 import com.example.scorereader.ui.reader.viewmodel.ReaderViewModel
 import androidx.compose.material3.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import java.io.File
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,98 +37,123 @@ fun ReaderScreen(filePath: String) {
     val fileName = remember(filePath) {
         File(filePath).nameWithoutExtension
     }
-
+    var isAnnotationMode by remember { mutableStateOf(false) }
+    var currentPath by remember { mutableStateOf(Path()) }
+    val paths = remember { mutableStateListOf<Path>() }
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {
-                    Text(text = fileName)
+                title = { Text(text = fileName) },
+                actions = {
+                    TextButton(onClick = {
+                        isAnnotationMode = !isAnnotationMode
+                    }) {
+                        Text(if (isAnnotationMode) "Leer" else "Anotar")
+                    }
                 }
+
             )
         }
     ) { paddingValues ->
-            val readerViewModel: ReaderViewModel = viewModel()
-            val currentPage by readerViewModel.currentPage.collectAsState()
-            val pageCount by readerViewModel.pageCount.collectAsState()
+        val readerViewModel: ReaderViewModel = viewModel()
+        val currentPage by readerViewModel.currentPage.collectAsState()
+        val pageCount by readerViewModel.pageCount.collectAsState()
 
-            var zoomTarget by remember { mutableStateOf(1f) }
-            val zoom by animateFloatAsState(
-                targetValue = zoomTarget,
-                label = "zoomAnimation"
-            )
-            val minZoom = 1f
-            val maxZoom = 3f
+        var zoomTarget by remember { mutableStateOf(1f) }
+        val zoom by animateFloatAsState(
+            targetValue = zoomTarget,
+            label = "zoomAnimation"
+        )
+        val minZoom = 1f
+        val maxZoom = 3f
 
+        var bitmap by remember { mutableStateOf<Bitmap?>(null) }
 
-            var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+        var twoPagesMode by remember { mutableStateOf(false) }
 
+        LaunchedEffect(filePath, currentPage) {
+            val file = File(filePath)
 
-            var twoPagesMode by remember { mutableStateOf(false) }
-
-            LaunchedEffect(filePath, currentPage) {
-                val file = File(filePath)
-
-                if (!file.exists()) {
-                    return@LaunchedEffect
-                }
-
-                val fileDescriptor = ParcelFileDescriptor.open(
-                    file,
-                    ParcelFileDescriptor.MODE_READ_ONLY
-                )
-
-                val renderer = PdfRenderer(fileDescriptor)
-                val totalPages = renderer.pageCount
-
-                readerViewModel.setPageCount(totalPages)
-
-                if (totalPages == 0) {
-                    renderer.close()
-                    fileDescriptor.close()
-                    return@LaunchedEffect
-                }
-
-                val safePageIndex = currentPage.coerceIn(0, totalPages - 1)
-                val page = renderer.openPage(safePageIndex)
-
-                val bmp = Bitmap.createBitmap(
-                    page.width,
-                    page.height,
-                    Bitmap.Config.ARGB_8888
-                )
-
-                page.render(bmp, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-                bitmap = bmp
-
-                page.close()
-                renderer.close()
-                fileDescriptor.close()
+            if (!file.exists()) {
+                return@LaunchedEffect
             }
 
-            bitmap?.let {
+            val fileDescriptor = ParcelFileDescriptor.open(
+                file,
+                ParcelFileDescriptor.MODE_READ_ONLY
+            )
+
+            val renderer = PdfRenderer(fileDescriptor)
+            val totalPages = renderer.pageCount
+
+            readerViewModel.setPageCount(totalPages)
+
+            if (totalPages == 0) {
+                renderer.close()
+                fileDescriptor.close()
+                return@LaunchedEffect
+            }
+
+            val safePageIndex = currentPage.coerceIn(0, totalPages - 1)
+            val page = renderer.openPage(safePageIndex)
+
+            val bmp = Bitmap.createBitmap(
+                page.width,
+                page.height,
+                Bitmap.Config.ARGB_8888
+            )
+
+            page.render(bmp, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            bitmap = bmp
+
+            page.close()
+            renderer.close()
+            fileDescriptor.close()
+        }
+
+        bitmap?.let {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+
+                // PDF
                 Image(
-                    bitmap = it.asImageBitmap(),
-                    contentDescription = "Página PDF",
+                    bitmap = bitmap!!.asImageBitmap(),
+                    contentDescription = null,
                     contentScale = ContentScale.Fit,
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(paddingValues)
-                        .graphicsLayer {
-                            scaleX = zoom
+                        .graphicsLayer(
+                            scaleX = zoom,
                             scaleY = zoom
-                        }
+                        )
                         .pointerInput(Unit) {
+
                             detectTransformGestures { _, _, zoomChange, _ ->
-                                zoomTarget = (zoomTarget * zoomChange).coerceIn(minZoom, maxZoom)
+
+                                val newZoom = (zoomTarget * zoomChange)
+                                    .coerceIn(minZoom, maxZoom)
+
+                                zoomTarget = newZoom
+                                readerViewModel.updateZoom(newZoom)
                             }
                         }
                         .pointerInput(Unit) {
+
                             detectTapGestures(
+
                                 onDoubleTap = {
-                                    zoomTarget = 1f  // reset
+                                    // resetear zoom
+                                    zoomTarget = 1f
+                                    readerViewModel.resetZoom()
                                 },
+
                                 onTap = { tapOffset ->
-                                    val screenWidth = size.width
+
+                                    val screenWidth = this.size.width
+
                                     if (tapOffset.x > screenWidth / 2) {
                                         readerViewModel.nextPage()
                                     } else {
@@ -130,8 +163,60 @@ fun ReaderScreen(filePath: String) {
                             )
                         }
                 )
+
+                // Canvas para anotaciones
+                Canvas(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                    paths.forEach { path ->
+                        drawPath(
+                            path = path,
+                            color = Color.Red,
+                            style = Stroke(width = 4f)
+                        )
+                    }
+
+                    drawPath(
+                        path = currentPath,
+                        color = Color.Red,
+                        style = Stroke(width = 4f)
+                    )
+                }
+
+                if (isAnnotationMode) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .pointerInput(Unit) {
+
+                                detectDragGestures(
+
+                                    onDragStart = { offset ->
+                                        currentPath = Path().apply {
+                                            moveTo(offset.x, offset.y)
+                                        }
+                                    },
+
+                                    onDrag = { change, _ ->
+
+                                        val newPath = Path().apply {
+                                            addPath(currentPath)
+                                            lineTo(change.position.x, change.position.y)
+                                        }
+
+                                        currentPath = newPath
+                                    },
+
+                                    onDragEnd = {
+                                        paths.add(currentPath)
+                                        currentPath = Path()
+                                    }
+                                )
+                            }
+                    )
+                }
             }
+        }
     }
-
-
 }
